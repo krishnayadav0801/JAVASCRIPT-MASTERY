e58e1759caf8a0070d8326133ee30154c5e714ea
krishnayadav0801 <krishnayadav08jan2006@gmail.com>
2025-11-14 10:34:21 +0000

    Data-Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all js code as newer version
+// alert("Hello Krishna ") // We are using nodejs not browser
+
+console.log(3 + 3);
+
+// Note :- Code Readability should be high.
+// Ecma script is an organisation who set standards of JavaScript. for example:- how to declare name of variables. 
+
+
+let name = "Krishna" // String
+let age = 19 // Number
+let isLoggedin = true // Boolean
+let temperature = null
+let user; // undefined (not defined)
+
+// PRIMITIVE DATA-TYPES
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => is a standalone value , but type of  null is object
+// undefined => 
+// symbol => unique 
+
+// NON-PRIMITIVE / REFERENCIVE  DATA-TYPE
+
+console.log(null); // object
+console.log(undefined); // undefined
+

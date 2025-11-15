0a3fbf91d102ff4f127d362167eb40bd33ddd8ec
krishnayadav0801 <krishnayadav08jan2006@gmail.com>
2025-11-15 07:02:33 +0000

    comparision operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,15 +1,61 @@
 let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof score);
+// console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // "33" => 33
-// "33abc"  => Nan
+// "33abc"  => NaN
+
+// ------------------++----------------------------
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2*3);
+// console.log(2/3);
+// console.log(2**3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " krishna"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 2 + "2");
+// console.log("1" + 2 + 2);
+
+
+console.log(true);
+// console.log(+true);  DON'T DO THIS SHIT
+// console.log(+""); DON'T DO THIS
+
+// let num1 , num2 , num3 
+//  num1 = num2 = num3 = 2 + 2     DON'T DO THIS
+
+
+//-------prefix operator & postfix operater -----------// 
+
+let gameCounter = 100
+console.log(++gameCounter);
+console.log(gameCounter++);
+console.log(gameCounter);
+
+
+
+
+
+
+
 
 
 
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+//-------------------comparision---------------------//
+/*
+
+console.log(2 > 1);
+console.log(2 < 1);
+console.log(2 >= 1);
+console.log(2 <= 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+*/
+
+// console.log("2" > 1);
+// console.log("2" >= 1);
+
+// === strict check
+
+console.log("2" === 2);
+
+
+

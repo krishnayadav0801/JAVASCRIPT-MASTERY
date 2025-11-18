d403e72371ec7d2f2366c0275029ce40c39177c7
krishnayadav0801 <krishnayadav08jan2006@gmail.com>
2025-11-18 07:06:59 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,30 @@
+const name = "Krishna "
+const repoCount = 3
+
+// console.log(name + repoCount + " I am Fine");  
+// console.log(` hello my name is ${name} my repo count is ${repoCount}` ); // string-eterpulation
+
+// another way of declaring the string
+const gameName = new String('krishna is a nice guy')  // new keyword means an empty object and then a srting inside it its like string-object with key value pair.
+
+// example
+// console.log(gameName[0])
+// console.log(gameName[1])
+// console.log(gameName[2])
+// console.log(gameName[3])
+// console.log(gameName[4])
+// console.log(gameName[5])
+// console.log(gameName[6])
+
+//------------------------
+// console.log(gameName.__proto__); //  object 
+
+// let newGameName = gameName.toUpperCase()
+// console.log(newGameName);
+// console.log(gameName);
+
+
+
+
+
+
